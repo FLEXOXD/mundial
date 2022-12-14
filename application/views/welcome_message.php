89ee909aa3f84f6f53945bb73853f9a6c36668fd
FLEXOXD <davidflexjoker@gmail.com>
2022-12-14 00:47:20 -0500

    imagenes y diseño pre eliminar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,9 +1,48 @@
 <div class="well text-center">
 	<h4>
-		<i class="glyphicon glyphicon-pencil"></i>
-		<b>MUNDIAL</b>
+		<i class="glyphicon glyphicon-home"></i>
+		<b>MUNDIAL 2022</b>
 	</h4>
-	<div class="text-center">
-		<img src="<?php echo base_url(); ?>/assets/images/mundial.jpg" alt="Logo" height="100px">
-	</div>
+</div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+		<li data-target="#myCarousel" data-slide-to="4"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="<?php echo base_url();?>/assets/images/4.jpg" height="250px" width="1850px">
+    </div>
+
+    <div class="item">
+      <img src="<?php echo base_url();?>/assets/images/5.jpg" height="250px" width="1850px">
+    </div>
+
+    <div class="item">
+      <img src="<?php echo base_url();?>/assets/images/3.jpg" height="250px" width="1850px">
+    </div>
+		<div class="item">
+      <img src="<?php echo base_url();?>/assets/images/2.jpg" height="250px" width="1850px">
+    </div>
+		<div class="item">
+      <img src="<?php echo base_url();?>/assets/images/1.jpg" height="250px" width="1850px">
+    </div>
+  </div>
+
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
