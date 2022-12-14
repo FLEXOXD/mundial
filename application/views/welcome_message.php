956da1d5b157a06cd20d1c93248d1f9be416ec48
FLEXOXD <davidflexjoker@gmail.com>
2022-12-14 00:37:35 -0500

    imagenes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,4 +3,7 @@
 		<i class="glyphicon glyphicon-pencil"></i>
 		<b>MUNDIAL</b>
 	</h4>
+	<div class="text-center">
+		<img src="<?php echo base_url(); ?>/assets/images/mundial.jpg" alt="Logo" height="100px">
+	</div>
 </div>
